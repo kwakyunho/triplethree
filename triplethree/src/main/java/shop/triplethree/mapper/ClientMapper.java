b00738425c39260082e5cbdea5dd3d97a52dce9b
smart-202-5 <smart-202-5@smart-202-5-PC>
2019-12-03 17:38:38 +0900

    [김다혜] 1203

--- triplethree/src/main/java/shop/triplethree/mapper/ClientMapper.java
+++ triplethree/src/main/java/shop/triplethree/mapper/ClientMapper.java
@@ -1,14 +1,25 @@
 package shop.triplethree.mapper;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 
 import shop.triplethree.vo.Client;
 
 @Mapper
 public interface ClientMapper {
+	//거래처(사업종류) 리스트 가져오기
+	public List<Client> getLicodeList();
+	
+	//거래처 정보 등록
 	public int clientInsert(Client client);
 	
+	//거래처 정보 목록가져오기
 	public List<Client> getClientList();
+	
+	//거래처 정보 하나만 가져오기(수정.삭제할때)
+	public Client getClientBycode(String code);
+	
+	//거래처 정보 수정하기
+	public int clientUpdate(Client client);
+	
 }
--- triplethree/src/main/java/shop/triplethree/service/ClientService.java
+++ triplethree/src/main/java/shop/triplethree/service/ClientService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import shop.triplethree.mapper.ClientMapper;
-import shop.triplethree.mapper.CommonMapper;
 import shop.triplethree.vo.Client;
 
 @Service
@@ -13,6 +12,7 @@ public class ClientService {
 	@Autowired private CommonService commonService;
 	@Autowired private ClientService clientService;
 	@Autowired private ClientMapper clientMapper;	
+	
 	//인서트를 위한 Pk 코드 생성 메서드 
 	public int createClientCode(Client client) {
 		String code = commonService.codeGeneration("ACC_MANAGE");
@@ -21,9 +21,27 @@ public class ClientService {
 		client.setWriter("id007");//세션에서 가져올 값 : 작성자 세팅
 		return clientMapper.clientInsert(client);
 	}
+	//거래처 정보 리스트로 뽑아오기
 	public List<Client> getClientList(){
 		List<Client> list = clientMapper.getClientList();
 		return list;
+	}
+	
+	//거래처 정보 하나만 (수정하기위해서 가져오는거)
+	public Client getClientBycode(String code){
+		Client client = clientMapper.getClientBycode(code);
+		return client;
+	}
+	
+	//거래처 사업장 종류 가져오기
+	public List<Client> getLicodeList(){
+		return clientMapper.getLicodeList();
+	}
+	
+	//거래처 정보 수정하기
+	public int clientUpdate(Client client) {
+		
+		return clientMapper.clientUpdate(client);
 	}
 	
 }
