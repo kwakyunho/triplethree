626eb538904f5df9dccd2e3be651c6a157e119d2
ME <ME@DESKTOP-3MFBSCT>
2019-12-04 23:50:31 +0900

    급여조회경로 이동 완료 수정화면이랑 만들고 조회화면은 급여대장처럼 보이게 만들자

--- triplethree/src/main/java/shop/triplethree/controller/PayController.java
+++ triplethree/src/main/java/shop/triplethree/controller/PayController.java
@@ -2,12 +2,22 @@ package shop.triplethree.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PayController {
+	
+	//급여등록화면
 	@GetMapping("/insertPay")
 	public String insertPay() {
 		System.out.println("급여지급등록화면");
 		return "pay/insertPay";		
 	}
+	
+	//사원 급여 조회
+	@PostMapping("/payList")
+	public String payList() {
+		return "/pay/payList";
+	}
+	
 }
