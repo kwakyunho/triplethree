b0a61dbba089acbe27e88965f44a9ee07c17d6be
JJ-HS <57096103+JJ-HS@users.noreply.github.com>
2019-12-11 14:56:21 +0900

    [정해성] 	차량 예약 화면 연결 완료

--- triplethree/src/main/java/shop/triplethree/controller/AssetManageController.java
+++ triplethree/src/main/java/shop/triplethree/controller/AssetManageController.java
@@ -1,8 +1,19 @@
 package shop.triplethree.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AssetManageController {
 
+	/**
+	 * 메뉴에서 차량 예약 눌렸을때 실행
+	 * 예약 리스트를 보여줌
+	 * */
+	@GetMapping("/assetsManageCaSelect")
+	public String selectCarManageAsset() {
+		System.out.println("차량 예약 신청이 클릭되었나요?");
+		
+		return "/assets/assetsCarManage";
+	}
 }
