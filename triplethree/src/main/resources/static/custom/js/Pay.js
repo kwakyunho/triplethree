e63621ab9abd4c632ecc207eebd59e38bedd32c9
smart-202-15 <smart-202-15@smart-202-15-PC>
2019-12-16 15:24:44 +0900

    [급여 제이쿼리 추가] 공제항목 언제하냐

--- triplethree/src/main/resources/static/custom/js/Pay.js
+++ triplethree/src/main/resources/static/custom/js/Pay.js
@@ -3,16 +3,40 @@ $(function(){
 	
 	$('.choiceBtn').on('click', function(){
 		var basicPay = $(this).parents('.insertPay').children('.basicPay');		
+		var beneFit = $(this).parents('.insertPay').children('.beneFit');		
+		var holidayPay = $(this).parents('.insertPay').children('.holidayPay');		
+		var overtimePay = $(this).parents('.insertPay').children('.overtimePay');		
+		var positionBenefit = $(this).parents('.insertPay').children('.positionBenefit');		
+		var selfDriPay = $(this).parents('.insertPay').children('.selfDriPay');		
+		var comPay = $(this).parents('.insertPay').children('.comPay');		
 		var mealCost = $(this).parents('.insertPay').children('.mealCost');		
+		var childPay = $(this).parents('.insertPay').children('.childPay');		
 		$('#basicPay').val(basicPay.html('input').val());
+		$('#beneFit').val(beneFit.html('input').val());
+		$('#holidayPay').val(holidayPay.html('input').val());
+		$('#overtimePay').val(overtimePay.html('input').val());
+		$('#positionBenefit').val(positionBenefit.html('input').val());
+		$('#selfDriPay').val(selfDriPay.html('input').val());
+		$('#comPay').val(comPay.html('input').val());
+		$('#childPay').val(childPay.html('input').val());
 		$('#mealCost').val(mealCost.html('input').val());
 	
 		//.children('input[type="hidden"]').val();
-		var basicPay=document.getElementById("basicPay");
-		var mealCost=document.getElementById("mealCost");
+		var basicPay=document.getElementById("basicPay"); //기본급
+		var beneFit=document.getElementById("beneFit");	  //야간근무수당
+		var holidayPay=document.getElementById("holidayPay");//휴일근무수당
+		var overtimePay=document.getElementById("overtimePay");//연장근무수당
+		var positionBenefit=document.getElementById("positionBenefit");//직책수당
+		var selfDriPay=document.getElementById("selfDriPay");//자가운전보조금
+		var comPay=document.getElementById("comPay");//통신수당
+		var childPay=document.getElementById("childPay");//육아수당
+		var mealCost=document.getElementById("mealCost");//식대
+		
 		var sum1=document.getElementById("sum1");
-		var add=parseInt(basicPay.value)+parseInt(mealCost.value);
+		var add=parseInt(basicPay.value)+parseInt(beneFit.value)+parseInt(holidayPay.value)
+		+parseInt(overtimePay.value)+parseInt(positionBenefit.value)+parseInt(selfDriPay.value)
+		+parseInt(comPay.value)+parseInt(childPay.value)+parseInt(mealCost.value);
 		sum1.value =add;
-	});
+	})
 	
 });
