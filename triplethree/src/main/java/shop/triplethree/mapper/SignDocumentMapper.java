2a79230c02b79b099aa80b713233916a26f8e58a
JJ-HS <57096103+JJ-HS@users.noreply.github.com>
2020-01-03 13:41:42 +0900

    [정해성] 결재대기함 데이터 테이블 오류 수정 완료 / 결재 순서에 맞게 결재대기문서 올수 있는 방법 생각해볼것

--- triplethree/src/main/java/shop/triplethree/mapper/SignDocumentMapper.java
+++ triplethree/src/main/java/shop/triplethree/mapper/SignDocumentMapper.java
@@ -12,8 +12,11 @@ public interface SignDocumentMapper {
 	// 기안 문서함에서 기안올린 문서를 select하는 메서드
 	public List<SignDocument> selectDraftDocument(String valCheck, String sid);
 	
-	// 결재 문서함에서 본인의 결재 건수가 있는 문서를 select하는 메서드
+	// 결재 대기함에서 본인의 결재 건수가 있는 문서를 select하는 메서드
 	public List<String> selectSignDocumentWait(String sid);
+	
+	// 결재 대기함에서 본인의 결재 순번을 가져오는 메서드
+	/* public int selectSignNum(String sid); */
 	
 	// 결재 문서함에서 상단 메뉴클릭에 따라 문서를 select하는 메서드
 	public List<SignDocument> selectSignDocumentEnd(String valCheck, String sid);
