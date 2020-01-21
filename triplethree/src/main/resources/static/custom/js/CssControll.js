706e87caeefd455ce6efc8b292caf7e0f576fcd4
kwakyunho <kwakyunho@nate.com>
2020-01-21 16:35:44 +0900

    [곽윤호] 페이지 이동시에도 메뉴 목록 active 활성화하기

--- triplethree/src/main/resources/static/custom/js/CssControll.js
+++ triplethree/src/main/resources/static/custom/js/CssControll.js
@@ -2,11 +2,17 @@
  * sidebar 클릭에 따라 active 유지하기
  */
 $(document).ready(function(){
+	var pathName = $(location).attr('pathname');
+	var pathNameUri = $(document).find('a[href="' + pathName + '"]');
+	pathNameUri.parents('li.has-treeview').addClass('menu-open');
+	pathNameUri.addClass('active');
+	
 	$(function(){
-		var sBtn = $(".nav-treeview");
-		sBtn.find("a").click(function(){
-			sBtn.find("a").removeClass("active");
-			$(this).addClass("active");
+		var sBtn = $('.nav-treeview');
+		
+		sBtn.find('a').click(function(){
+			sBtn.find('a').removeClass('active');
+			$(this).addClass('active');
 		});
 	});
 });
